039725d7850d9285cde433076ea382a547a1b457
Ather1230 <ather.biswas0057@gmail.com>
2022-10-21 02:05:33 +0600

    Ui completed

--- src/Pages/Shared/BrandCarousel/BrandCoursel.js
+++ src/Pages/Shared/BrandCarousel/BrandCoursel.js
@@ -13,10 +13,6 @@ const BrandCoursel = () => {
           src={Brand1}
           alt="First slide"
         />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
@@ -24,11 +20,6 @@ const BrandCoursel = () => {
           src={Brand2}
           alt="Second slide"
         />
-
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
     );
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,46 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import LeftSideNav from '../LeftSideNav/LeftSideNav';
+import { FaUserCircle } from 'react-icons/fa';
+
 
 const Header = () => {
     return (
-        <div>
-            <h3>This is header</h3>
-        </div>
+        <Navbar className='mb-3 shadow' collapseOnSelect expand="lg" bg="light" variant="light">
+      <Container>
+        <Navbar.Brand href="#home">Anaconda-News</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">All News</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              <FaUserCircle></FaUserCircle>
+            </Nav.Link>
+          </Nav>
+          <div className='d-lg-none'>
+            <LeftSideNav></LeftSideNav>
+          </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     );
 };
 
